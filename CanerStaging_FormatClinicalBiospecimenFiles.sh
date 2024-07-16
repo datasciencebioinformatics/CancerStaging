e520b2cefb259d655754c427686af8a3e547b856
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-16 11:59:04 -0500

    Update CanerStaging_FormatClinicalBiospecimenFiles.sh

--- CanerStaging_FormatClinicalBiospecimenFiles.sh
+++ CanerStaging_FormatClinicalBiospecimenFiles.sh
@@ -1,6 +1,6 @@
 # Organize bioscpecimen and clinical txt data
-cp /home/felipe/Downloads/biospecimen.project-tcga-brca.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
-cp /home/felipe/Downloads/clinical.project-tcga-brca.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/biospecimen.cohort.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/clinical.cohort.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
 cp /home/felipe/Downloads/gdc_manifest.2024-07-16.txt /home/felipe/googledrive/Cancer_staging/
 cp /home/felipe/Downloads/gdc_sample_sheet.2024-07-16.txt /home/felipe/googledrive/Cancer_staging/
 cp /home/felipe/Downloads/metadata.cohort.2024-07-16.txt /home/felipe/googledrive/Cancer_staging/
