ac2b50db2bc5bea1fec4b73ed04706f167d5a1ed
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-18 13:25:54 -0500

    Update CanerStaging_FormatClinicalBiospecimenFiles.sh

--- CanerStaging_FormatClinicalBiospecimenFiles.sh
+++ CanerStaging_FormatClinicalBiospecimenFiles.sh
@@ -1,11 +1,10 @@
 # Organize bioscpecimen and clinical txt data
-cp /home/felipe/Downloads/biospecimen.cohort.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
-cp /home/felipe/Downloads/clinical.cohort.2024-07-16.tar.gz /home/felipe/googledrive/Cancer_staging/
-cp /home/felipe/Downloads/gdc_manifest.2024-07-16.txt /home/felipe/googledrive/Cancer_staging/
-cp /home/felipe/Downloads/gdc_sample_sheet.2024-07-16.tsv /home/felipe/googledrive/Cancer_staging/
-cp /home/felipe/Downloads/metadata.cohort.2024-07-16.txt /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/biospecimen.cohort.2024-07-18.tar.gz /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/clinical.cohort.2024-07-18.tar.gz /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/gdc_manifest.2024-07-18.txt /home/felipe/googledrive/Cancer_staging/
+cp /home/felipe/Downloads/gdc_sample_sheet.2024-07-18.tsv /home/felipe/googledrive/Cancer_staging/
 
 
 # Untar the zip files
-tar xzf /home/felipe/googledrive/Cancer_staging/biospecimen.cohort.2024-07-16.tar.gz -C /home/felipe/googledrive/Cancer_staging/
-tar xzf /home/felipe/googledrive/Cancer_staging/clinical.cohort.2024-07-16.tar.gz -C  /home/felipe/googledrive/Cancer_staging/
+tar xzf /home/felipe/googledrive/Cancer_staging/biospecimen.cohort.2024-07-18.tar.gz -C /home/felipe/googledrive/Cancer_staging/
+tar xzf /home/felipe/googledrive/Cancer_staging/clinical.cohort.2024-07-18.tar.gz -C  /home/felipe/googledrive/Cancer_staging/
