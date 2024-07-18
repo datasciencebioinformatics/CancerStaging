33fd25b999b8cdea22295513ad320be16a0e69c4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-18 12:24:06 -0500

    Update CancerStaging_CreateMetadataFromGDCFiles.R

--- CancerStaging_CreateMetadataFromGDCFiles.R
+++ CancerStaging_CreateMetadataFromGDCFiles.R
@@ -29,12 +29,12 @@ gdc_sample_sheet_data$sample_id<-gsub(".rna_seq.augmented_star_gene_counts.tsv",
 # primary_diagnosis
 # site_of_resection_or_biopsy
 #cat /home/felipe/googledrive/Cancer_staging/clinical.tsv | sed "s/'--/ /g" > /home/felipe/googledrive/Cancer_staging/clinical.txt
-#cat /home/felipe/googledrive/Cancer_staging/sample.tsv | sed "s/'--/ /g" > /home/felipe/googledrive/Cancer_staging/clinical.txt
+#cat /home/felipe/googledrive/Cancer_staging/sample.tsv | sed "s/'--/ /g" > /home/felipe/googledrive/Cancer_staging/sample.txt
 #cat /home/felipe/googledrive/Cancer_staging/exposure.tsv | sed "s/'--/ /g" > /home/felipe/googledrive/Cancer_staging/exposure.txt
 
 # Set path to files                                                                                                 
 clinical_file="/home/felipe/googledrive/Cancer_staging/clinical.txt" 
-sample_file="/home/felipe/googledrive/Cancer_staging/clinical.txt"    
+sample_file="/home/felipe/googledrive/Cancer_staging/sample.txt"    
 exposure_file="/home/felipe/googledrive/Cancer_staging/exposure.txt"                                                  
 
 # Load data
@@ -51,10 +51,30 @@ merged_sample_clinical_data<-merge(merged_sample_clinical_data,exposure_data,by=
 # Merge tables
 merged_data_patient_info<-merge(merged_sample_clinical_data,gdc_sample_sheet_data,by.x="sample_submitter_id.x",by.y="sample_submitter_id" )
 #####################################################################################################################
-sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Primary Tumor") # Number of Primary Tumor
-sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Solid Tissue Normal") # Number of Solid Tissue Normal
-sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Metastatic") # Number of Solid Tissue Normal
+sum(unique(merged_data_patient_info[,c("sample_id.x","Sample.Type")])[,2]=="Primary Tumor") # Number of Primary Tumor
+sum(unique(merged_data_patient_info[,c("sample_id.x","Sample.Type")])[,2]=="Solid Tissue Normal") # Number of Solid Tissue Normal
+sum(unique(merged_data_patient_info[,c("sample_id.x","Sample.Type")])[,2]=="Metastatic") # Number of Solid Tissue Normal
 
+# A field to store 
+merged_data_patient_info$stages<-merged_data_patient_info$ajcc_pathologic_stage
+
+# Group stages I,II,III and IV
+merged_data_patient_info$stages<-gsub("Stage IA", "Stage I", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IB", "Stage I", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIA", "Stage II", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIB", "Stage II", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIC", "Stage II", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIIA", "Stage III", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIIB", "Stage III", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IIIC", "Stage III", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IVA", "Stage IV", merged_data_patient_info$stages)
+merged_data_patient_info$stages<-gsub("Stage IVB", "Stage IV", merged_data_patient_info$stages)
+
+# Cases per stage
+table_cases_per_stage<-table(merged_data_patient_info$project_id, merged_data_patient_info$stages)
+
+# Cases per stage
+table_cases_per_stage<-table_cases_per_stage[,c("Stage I","Stage II","Stage III","Stage IV")]
 
 # table(unique(merged_data_patient_info[,c("sample_id","ethnicity")])$ethnicity)
 # table(unique(merged_data_patient_info[,c("sample_id","gender")])$gender)
