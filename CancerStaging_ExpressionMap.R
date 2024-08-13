90a35ed60309f808f608561aa98cff4a9608bfcc
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-13 09:45:33 -0500

    Create CancerStaging_ExpressionMap.R

---
+++ CancerStaging_ExpressionMap.R
@@ -0,0 +1,14 @@
+# Construnction of 3d coordinates
+# X=stages
+# Y=connectivity
+# Z=expression
+
+# Input X and Z.
+# Table with expression of each gene in stages I, stage II and stage III
+# For each gene, I have the epxression in all samples
+# Each sample is attributed to one specific stage
+
+# Input Y.
+# Conectivity 
+# The number of interaction-partners a gene has in the interactome
+
