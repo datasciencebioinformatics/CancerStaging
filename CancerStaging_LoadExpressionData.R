60169695bc52adf620796fddf33e5f47028748dc
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-16 12:42:55 -0500

    Update CancerStaging_LoadExpressionData.R

--- CancerStaging_LoadExpressionData.R
+++ CancerStaging_LoadExpressionData.R
@@ -1,10 +1,23 @@
 #####################################################################################################################
 # This script will take the TSV file (metadata), unstranded.rna_seq.augmented_star_gene_counts (rna-seq count data), 
-# the name of genes and samples already processed for the primary_diagnosis=Squamous cell carcinoma, NOS
 #####################################################################################################################
 # Reading the contents of TSV file using read_tsv() method
 merged_data_patient_info_file<- "/home/felipe/googledrive/Cancer_staging/merged_data_patient_info.tsv"
 
 # Load metadata table
 merged_data_patient_info     <-read.table(file = merged_data_patient_info_file, sep = '\t', header = TRUE,fill=TRUE)   
+#####################################################################################################################
+# A script to load cancer data base in R
+unstranded_file       <- "/home/felipe/googledrive/Cancer_staging/tables/unstranded.rna_seq.augmented_star_gene_counts.tsv"
+colnames_file         <- "/home/felipe/googledrive/Cancer_staging/tables/header.txt"
+rownames_file         <- "/home/felipe/googledrive/Cancer_staging/tables/gene_ids.txt"
+
+# Load data
+unstranded_data<-read.table(file = unstranded_file, sep = '\t', header = FALSE,fill=TRUE)    
+colnames_data<-read.table(file = colnames_file, sep = '\t', header = FALSE,fill=TRUE)                                    
+rownames_data<-read.table(file = rownames_file, sep = '\t', header = FALSE,fill=TRUE)     
+
+# Set colnames and rownames
+rownames(unstranded_data)<-rownames_data[,1]
+colnames(unstranded_data)<-colnames_data[,1]
 #####################################################################################################################
