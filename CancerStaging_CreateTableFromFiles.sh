58cac023f928195d68fb5e4fab3beef03ecaf2fd
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-16 05:57:47 -0500

    Update CancerStaging_CreateTableFromFiles.sh

--- CancerStaging_CreateTableFromFiles.sh
+++ CancerStaging_CreateTableFromFiles.sh
@@ -24,17 +24,19 @@ input_folder=/home/felipe/googledrive/Cancer_staging/tables/
 ls  /home/felipe/googledrive/Cancer_staging/tables/*/*.rna_seq.augmented_star_gene_counts.tsv  | while read file
 do
     folder_name=$(echo $file  | sed 's/\// /g' | awk '{print "/"$1"/"$2"/"$3"/"$4"/"$5"/"$6"/"}')
-    file_name=$(echo $file  | sed 's/\// /g' | awk '{print $7}')
+    file_name_2=$(echo $file  | sed 's/\// /g' |  sed 's/\./ /g' | awk '{print $7}')    
+    file_name=$(echo $file  | sed 's/\// /g' | awk '{print $7}')    
     file_path=$(echo $folder_name"/"$file_name)    
-    echo $file_path
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $1}' > $folder_name$file_name".gene_id.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $2}' > $folder_name$file_name".gene_name.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $3}' > $folder_name$file_name".gene_typec.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $4}' > $folder_name$file_name".unstranded.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $5}' > $folder_name$file_name".stranded_first.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $6}' > $folder_name$file_name".stranded_second.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $7}' > $folder_name$file_name".tpm_unstranded.txt"
-    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $8}' > $folder_name$file_name".fpkm_unstranded.txt"    
+    echo $file_name
+    echo $folder_name$file_name_2".gene_id.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $1}' > $folder_name$file_name_2".gene_id.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $2}' > $folder_name$file_name_2".gene_name.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $3}' > $folder_name$file_name_2".gene_typec.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $4}' > $folder_name$file_name_2".unstranded.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $5}' > $folder_name$file_name_2".stranded_first.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $6}' > $folder_name$file_name_2".stranded_second.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $7}' > $folder_name$file_name_2".tpm_unstranded.txt"
+    cat $file_path | grep -v GENCODE | grep -v gene_id | grep -v N_unmapped | grep -v N_multimapping | grep -v N_noFeature | grep -v N_ambiguous | awk '{print $8}' > $folder_name$file_name_2".fpkm_unstranded.txt"    
 done
 # Finish here, to do
 # Add collumn to the table
