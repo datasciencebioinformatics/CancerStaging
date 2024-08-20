47a113905fb7d52ec8d812746b3aa44c62eed673
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-20 13:29:05 -0500

    Update CancerStaging_LoadInteractomeData.R

--- CancerStaging_LoadInteractomeData.R
+++ CancerStaging_LoadInteractomeData.R
@@ -4,10 +4,10 @@
 #######################################################################################################################################
 # Path to input files
 # Interactome file
-interactome_file<-"/home/felipe/Documentos/LungPortal/Full_Interactome_Flavia.txt"
+interactome_file<-"/home/felipe/Documents/github/CancerStaging/Full_Interactome_Flavia.txt"
 
 # EnsemblToUniprotKBconversionList file
-EnsemblToUniprotKBconversionList_file<-"/home/felipe/Documentos/LungPortal/EnsemblToUniprotKBconversionList.txt"
+EnsemblToUniprotKBconversionList_file<-"/home/felipe/Documents/github/CancerStaging/EnsemblToUniprotKBconversionList.txt"
 #######################################################################################################################################
 # Read input table
 # Gene table
@@ -19,47 +19,6 @@ EnsemblToUniprotKBconversionList_data <-read.table(file = EnsemblToUniprotKBconv
 # Rename collumns 
 colnames(interactome_data)<-c("Gene1","Gene2")
 #######################################################################################################################################
-# File path to gene stages
-# File path to gene stages
-# Version 1
-#file_genes_Stage_I   <-   paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_I.tsv",sep="")
-#file_genes_Stage_II   <-  paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_II.tsv",sep="")
-#file_genes_Stage_III   <- paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_III.tsv",sep="")
-
-file_genes_Stage_I     <-   paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_Stage_","sample_stage_I",".tsv",sep="")
-file_genes_Stage_II    <-   paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_Stage_","sample_stage_II",".tsv",sep="")
-file_genes_Stage_III   <-   paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_Stage_","sample_stage_III",".tsv",sep="")
-
-# Gene table
-genes_Stage_I       <-read.table(file = file_genes_Stage_I, sep = '\t', header = TRUE,fill=TRUE)         
-genes_Stage_II      <-read.table(file = file_genes_Stage_II, sep = '\t', header = TRUE,fill=TRUE)
-genes_Stage_III     <-read.table(file = file_genes_Stage_III, sep = '\t', header = TRUE,fill=TRUE)   
-########################################################################################################################################
-# Store genes stage I, II and III
-# Vectors to store gene ids from each stage
-genes_id_vector_stage_I<-c()
-genes_id_vector_stage_II<-c()
-genes_id_vector_stage_III<-c()
-
-# For each gene in stage I
-for (gene_id in genes_Stage_I$gene)
-{
-  # Store gene id in the vector
-  genes_id_vector_stage_I<-c(genes_id_vector_stage_I,strsplit(gene_id, split = "\\.")[[1]][1])
-}
-# For each gene in stage II
-for (gene_id in genes_Stage_II$gene)
-{
-  # Store gene id in the vector
-  genes_id_vector_stage_II<-c(genes_id_vector_stage_II,strsplit(gene_id, split = "\\.")[[1]][1])
-}
-# For each gene in stage III
-for (gene_id in genes_Stage_III$gene)
-{
-  # Store gene id in the vector
-  genes_id_vector_stage_III<-c(genes_id_vector_stage_III,strsplit(gene_id, split = "\\.")[[1]][1])
-}
-#######################################################################################################
 # Filter tables to keep only the gene entries that are listed in the EnsemblToUniprotKBconversionList
 interactome_data<-interactome_data[interactome_data$Gene1 %in% EnsemblToUniprotKBconversionList_data$SYMBOL,]
 interactome_data<-interactome_data[interactome_data$Gene2 %in% EnsemblToUniprotKBconversionList_data$SYMBOL,]
