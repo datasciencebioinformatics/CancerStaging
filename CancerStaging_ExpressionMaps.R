384ef0e6f46bea2fcf17742999091c07ef58e613
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-20 13:29:30 -0500

    Update CancerStaging_ExpressionMaps.R

--- CancerStaging_ExpressionMaps.R
+++ CancerStaging_ExpressionMaps.R
@@ -1,17 +1,4 @@
 # Construnction of 3d coordinates
-# X=stages
-# Y=connectivity
-# Z=expression       
-
-# Input X and Z.
-# Table with expression of each gene in stages I, stage II and stage III
-# For each gene, I have the epxression in all samples
-# Each sample is attributed to one specific stage
-
-# Input Y.
-# Conectivity 
-# The number of interaction-partners a gene has in the interactome
-
-# Contourplot
-# Contourplot with ggplot2
-
+# X=stages : merged_data_patient_info_merged
+# Y=connectivity : interactome_data
+# Z=expression : reads_count_all_projects
