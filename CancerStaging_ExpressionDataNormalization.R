c774b7cf01b84c0caf4fca8dcd3a43749bb1b224
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-21 12:42:03 -0500

    Update CancerStaging_ExpressionDataNormalization.R

--- CancerStaging_ExpressionDataNormalization.R
+++ CancerStaging_ExpressionDataNormalization.R
@@ -1,3 +1,26 @@
 ####################################################################################################################
 # A script to normalize reads count to RPKM                                                                        #
 ####################################################################################################################
+# 
+# Compute gene length with xDb.Hsapiens.UCSC.hg19.knownGene
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+tx_by_gene <- transcriptsBy(txdb, by="gene")
+gene_lens <- max(width(tx_by_gene))
+
+# Compute gene getGeneLengthAndGCContent
+getGeneLengthAndGCContent("ENSG00000012048", "hsa")
+
+# edgeR rpkm
+y <- DGEList(counts=counts,genes=data.frame(Length=GeneLength))
+y <- calcNormFactors(y)
+RPKM <- rpkm(y)
+
+
+
+# deseq2 fpkm
+dds <- DESeqDataSetFromMatrix(countData = cts, colData = NULL, design= ~ 1)
+dds <- DESeq(dds)
+resultsNames(dds) # lists the coefficients
+# the FPKM values
+fpkm(dds)
