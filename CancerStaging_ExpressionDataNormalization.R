98df9ea511e18a6a4bb380f29fc00adce87f8979
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-23 11:43:22 -0500

    Update CancerStaging_ExpressionDataNormalization.R

--- CancerStaging_ExpressionDataNormalization.R
+++ CancerStaging_ExpressionDataNormalization.R
@@ -13,6 +13,43 @@ gene_lens <- max(width(tx_by_gene))                                             
 # Compute gene getGeneLengthAndGCContent                                                                           #
 getGeneLengthAndGCContent("ENSG00000012048", "hsa")                                                                #
 ####################################################################################################################
+
+
+####################################################################################################################
+# RPKM normalization                                                                                               #
+# The normalization is done for each 1000 genes duo to limitation in the biomart connection                        #
+# First, gene length and gc content for all genes in the reads count table                                         #
+# Take the gene names, without variant identification                                                              #
+# vector to store all gene ids                                                                                     #
+df_gene_ids<-data.frame(gene_id=c(),gene_id_cp=c())                                                                #
+for (gene_id in rownames(unstranded_data))                                                                         #
+{                                                                                                                  #
+    # Store gene ids                                                                                               #
+    print(gene_id)
+    gene_ids<-strsplit(gene_id,".",fixed=T)[[1]][[1]]                                                              #                                                  
+                                                                                                                   #
+    # Contatenate gene lists                                                                                       #
+    df_gene_ids<-rbind(df_gene_ids,data.frame(gene_id=gene_ids,gene_id_cp=gene_id))                                #
+}                                                                                                                  #
+# Split gene_ids vector in parts                                                                                   #
+gene_ids_vector<-split(df_gene_ids$gene_id,ceiling(seq_along(df_gene_ids$gene_id) / 1000))                         #
+####################################################################################################################
+# Data.frame to store geneLengthAndGCContent                                                                       #
+df_geneLengthAndGCContent<-data.frame(length=c(),gc=c())                                                           #
+                                                                                                                   #
+# For each part of the vectors                                                                                     #
+for (index in names(gene_ids_vector) )                                                                             #
+{                                                                                                                  #
+    # Concatenate files                                                                                            ########
+    df_geneLengthAndGCContent<-rbind(df_geneLengthAndGCContent,getGeneLengthAndGCContent(gene_ids_vector[[index]], "hsa"))#
+}                                                                                                                         #
+rownames(df_geneLengthAndGCContent)[!grepl(".", rownames(df_geneLengthAndGCContent), fixed=TRUE)]                         #
+###########################################################################################################################
+
+
+
+
+
 # edgeR rpkm
 y <- DGEList(counts=counts,genes=data.frame(Length=GeneLength))
 y <- calcNormFactors(y)
