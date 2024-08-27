87efa3987d81904f3280396d2890db3c7651c4ed
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-27 15:10:13 -0500

    Update CancerStaging_FindTumorGenes.R

--- CancerStaging_FindTumorGenes.R
+++ CancerStaging_FindTumorGenes.R
@@ -6,8 +6,8 @@ unstranded_NOISeq_rpkm_data <- unstranded_NOISeq_rpkm
 unstranded_NOISeq_TMM       <- unstranded_NOISeq_TMM
 ###########################################################################################################################
 # All tumor and control samples
-unpaired_tumor_samples  <-merged_data_patient_info_unique[merged_data_patient_info_count$Sample.Type=="Primary Tumor","sample_id"]
-unpaired_control_samples<-merged_data_patient_info_unique[merged_data_patient_info_count$Sample.Type=="Solid Tissue Normal","sample_id"]
+unpaired_tumor_samples  <-merged_data_patient_info_count[merged_data_patient_info_count$Sample.Type=="Primary Tumor","sample_id"]
+unpaired_control_samples<-merged_data_patient_info_count[merged_data_patient_info_count$Sample.Type=="Solid Tissue Normal","sample_id"]
 
 # Paired samples only
 paired_tumor_samples    <- paired_sample_df$normal
