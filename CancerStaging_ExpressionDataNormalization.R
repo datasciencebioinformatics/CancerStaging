38083b250d46fb701ae926af753abc669d4cdfbe
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-08-29 11:18:10 -0500

    Update CancerStaging_ExpressionDataNormalization.R

--- CancerStaging_ExpressionDataNormalization.R
+++ CancerStaging_ExpressionDataNormalization.R
@@ -87,38 +87,8 @@ geneLength_ENTREZID_ENSEMBL<-geneLength_ENTREZID_ENSEMBL[rownames(df_reads_count
 unstranded_dgelist              <- DGEList(counts=df_reads_count_all_projects_raw[geneLength_ENTREZID_ENSEMBL$ENSEMBL,],genes=data.frame(Length=geneLength_ENTREZID_ENSEMBL$geneLength))
 unstranded_dgelist              <- calcNormFactors(unstranded_dgelist, method = c("TMM"))
 df_reads_count_all_projects_tmm <- data.frame(cpm(unstranded_dgelist))
-##########################################################################################################################
-# RPKM normalization                                                                                               #
-# The normalization is done for each 1000 genes duo to limitation in the biomart connection                        #
-# First, gene length and gc content for all genes in the reads count table                                         #
-# Take the gene names, without variant identification                                                              #
-# vector to store all gene ids                                                                                     #
-df_gene_ids<-data.frame(gene_id=c(),gene_id_cp=c())                                                                #
-for (gene_id in rownames(df_reads_count_all_projects_raw))                                                                         #
-{                                                                                                                  #
-    # Store gene ids                                                                                               #
-    print(gene_id)
-    gene_ids<-strsplit(gene_id,".",fixed=T)[[1]][[1]]                                                              #                                                  
-                                                                                                                   #
-    # Contatenate gene lists                                                                                       #
-    df_gene_ids<-rbind(df_gene_ids,data.frame(gene_id=gene_ids,gene_id_cp=gene_id))                                #
-}                                                                                                                  #
-####################################################################################################################
-# Split gene_ids vector in parts                                                                                   #
-gene_ids_vector<-split(df_gene_ids$gene_id,ceiling(seq_along(df_gene_ids$gene_id) / 1000))                         #
-####################################################################################################################
-# Data.frame to store geneLengthAndGCContent                                                                       #
-df_geneLengthAndGCContent<-data.frame(length=c(),gc=c())                                                           #
-                                                                                                                   #
-# For each part of the vectors                                                                                     #
-for (index in names(gene_ids_vector) )                                                                             #
-{                                                                                                                  #
-    # Concatenate files                                                                                            ########
-    df_geneLengthAndGCContent<-rbind(df_geneLengthAndGCContent,getGeneLengthAndGCContent(gene_ids_vector[[index]], "hsa"))#
-}                                                                                                                         #
-rownames(df_geneLengthAndGCContent)[!grepl(".", rownames(df_geneLengthAndGCContent), fixed=TRUE)]                         #
 #############################################################################################################################
-df_reads_count_all_projects_rpkm<-data.frame(edgeR::rpkm(df_reads_count_all_projects_raw[rownames(df_geneLengthAndGCContent),], gene.length = df_geneLengthAndGCContent$length)) #
+df_reads_count_all_projects_rpkm<-data.frame(edgeR::rpkm(df_reads_count_all_projects_raw[rownames(geneLength_ENTREZID_ENSEMBL),], gene.length = geneLength_ENTREZID_ENSEMBL$geneLength)) #
 ##########################################################################################################################
 write_tsv(df_reads_count_all_projects_tmm, "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_tmm.tsv")   #
 write_tsv(data.frame(df_reads_count_all_projects_rpkm), "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_rpkm.tsv") #
