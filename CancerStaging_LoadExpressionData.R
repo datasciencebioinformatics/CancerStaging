dde8cf31fe492d2c7489960c0ea22151eba5391b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-01 03:55:30 -0500

    Update CancerStaging_LoadExpressionData.R

--- CancerStaging_LoadExpressionData.R
+++ CancerStaging_LoadExpressionData.R
@@ -152,10 +152,6 @@ reads_count_per_project_tpm[["TCGA-STAD"]])
 write.table(df_reads_count_all_projects_raw, "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_raw.tsv", na = "NA", append = TRUE, col.names = TRUE, row.names = TRUE, sep = "\t", quote = TRUE)
 write.table(df_reads_count_all_projects_fpkm, "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_fpkm.tsv", na = "NA", append = TRUE, col.names = TRUE, row.names = TRUE, sep = "\t", quote = TRUE)
 write.table(df_reads_count_all_projects_tpm, "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_tpm.tsv", na = "NA", append = TRUE, col.names = TRUE, row.names = TRUE, sep = "\t", quote = TRUE)
-
-save(df_reads_count_all_projects_raw, file = "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_raw.RData")
-save(df_reads_count_all_projects_fpkm, file = "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_fpkm.RData")
-save(df_reads_count_all_projects_tpm, file = "/home/felipe/Documents/Cancer_staging/df_reads_count_all_projects_tpm.RData")
 ###########################################################################################################################
 
 
