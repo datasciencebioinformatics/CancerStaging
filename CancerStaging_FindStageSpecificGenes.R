832df26f1091bb2a8bea838fbc411b7b9dccc7e4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-01 04:34:13 -0500

    Update CancerStaging_FindStageSpecificGenes.R

--- CancerStaging_FindStageSpecificGenes.R
+++ CancerStaging_FindStageSpecificGenes.R
@@ -96,9 +96,8 @@ for (normalization_scheme in normalization_schemes)
 		####################################################################################################################	
 		# Save TSV file with genes from Stage3
 		write_tsv(na.omit(log2change_Stage_i[selected_genes,]), paste(output_dir,"FindStageSpecificGenes_",FindStageSpecificGenes,"_",Stage_i,".tsv",sep=""))			
-		####################################################################################################################			
-		#######################################################################################################################################
-		list_stage_specific_genes[[Stage_i]]<-log2change_Stage_i
+		####################################################################################################################		
+		list_stage_specific_genes[[paste(normalization_scheme,"_",substring(Stage_i,8,14),sep="")]]<-log2change_Stage_i
 		####################################################################################################################	
 		cat(print(paste("\nNumber of tumor genes per stage for ",FindStageSpecificGenes,":",Stage_i, " : ",length(selected_genes))),file=paste(output_dir,"outfile.txt",sep="/"),append=TRUE)    
 	}			
