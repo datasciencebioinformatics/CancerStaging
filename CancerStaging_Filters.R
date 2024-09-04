d956995a9c52823eba1eb2c7ef273d618cce0496
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-04 16:22:40 -0500

    Update CancerStaging_Filters.R

--- CancerStaging_Filters.R
+++ CancerStaging_Filters.R
@@ -9,13 +9,13 @@ for (normalization_scheme in normalization_schemes)
 	tumor_genes<-normalized_statistic_table[normalized_statistic_table$tumor_genes == "yes","gene"]
 	
 	# First, I will load the expression table   	
-	normalized_expression_table<-normalized_expression_table_list[[normalization_scheme]]
+	normalized_expression_table<-df_reads_count_all_projects[[normalization_scheme]]
 	
 	# normalized_expression_table
 	normalized_expression_table<-normalized_expression_table[tumor_genes,]
 	
 	# Select only tumor genes
-	normalized_expression_table_list[[normalization_scheme]]<-normalized_expression_table
+	df_reads_count_all_projects[[normalization_scheme]]<-normalized_expression_table
 	
-	cat(print(paste("\nNumber of up-regulated filtered tumor-genes :",dim(normalized_expression_table_list[[normalization_scheme]])[1])),file=paste(output_dir,"outfile.txt",sep="/"),append=TRUE)
+	cat(print(paste("\nNumber of up-regulated filtered tumor-genes :",dim(df_reads_count_all_projects[[normalization_scheme]])[1])),file=paste(output_dir,"outfile.txt",sep="/"),append=TRUE)
 }
