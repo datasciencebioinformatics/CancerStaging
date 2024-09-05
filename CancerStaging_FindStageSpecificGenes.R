3aa4fbaa1e408ab5b78ec378e278498e7206e876
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-05 09:36:37 -0500

    Update CancerStaging_FindStageSpecificGenes.R

--- CancerStaging_FindStageSpecificGenes.R
+++ CancerStaging_FindStageSpecificGenes.R
@@ -3,7 +3,7 @@
 loaded_ExpressionTable         <-load(paste(output_dir,"/","ExpressionData.RData",sep="")) 
 loaded_MergedDataPatientInfo   <-load(paste(output_dir,"/","MergedDataPatientInfo.RData",sep="")) 
 loaded_MergedDataPatientInfo   <-load(paste(output_dir,"/","MergedDataPatientInfo.RData",sep="")) 
-
+loaded_StatisticTable          <-load(paste(output_dir,"/","StatisticTable.RData",sep="")) 
 #############################################################################################################################
 # A vector with the name of the normalizaton schemes
 normalization_schemes <- c("raw","rpkm","fpkm","tpm","tmm")
