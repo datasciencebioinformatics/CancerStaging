1324734e0a642bfb02daa970a4ae4db30bd8a631
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-06 07:17:36 -0500

    Update CancerStaging_SetupAllParamters.R

--- CancerStaging_SetupAllParamters.R
+++ CancerStaging_SetupAllParamters.R
@@ -17,7 +17,7 @@ threshold_FDR<-0.05
 threshold_tumor<-0.0 
 
 # rpkm=0,fpkm=0,tmm=0, tpm=0
-list_threshold_filters<-list(raw=0,rpkm=0,fpkm=0,tmm=0, tpm=0)
+list_threshold_filters<-list(raw=4,rpkm=4,fpkm=4,tmm=4, tpm=4)
 
 # log2foldchange threshold for the comparisson all tumor samples / all control samples
 threshold_stage<-1.0 
