6308165a3ebc8100583c99ab909ad090b5054f25
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-07 08:33:26 -0500

    Update CancerStaging_CaclulateShannonEntropy.R

--- CancerStaging_CaclulateShannonEntropy.R
+++ CancerStaging_CaclulateShannonEntropy.R
@@ -133,6 +133,6 @@ for (normalization_scheme in normalization_schemes)
 	}	  	  	
 ########################################################################################################################################		
 print(paste(normalization_scheme," : Stage I :", Entropy_stage_I_value_Carels, " : Stage II :", Entropy_stage_II_value_Carels, " : Stage III :", Entropy_stage_III_value_Carels, sep="") )
-cat(print(paste(normalization_scheme," : Stage I :", Entropy_stage_I_value_Carels, " : Stage II :", Entropy_stage_II_value_Carels, " : Stage III :", Entropy_stage_III_value_Carels, sep="") ),file=paste(output_dir,"outfile_1.txt",sep="/"),append=TRUE)
+cat(print(paste("\n",normalization_scheme," : Stage I :", Entropy_stage_I_value_Carels, " : Stage II :", Entropy_stage_II_value_Carels, " : Stage III :", Entropy_stage_III_value_Carels,"\n", sep="") ),file=results_files,append=TRUE)
 	
 }
