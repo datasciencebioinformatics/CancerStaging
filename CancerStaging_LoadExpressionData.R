b6f0361e0bad3d8ff303a663fd019491c1466810
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-07 08:03:26 -0500

    Update CancerStaging_LoadExpressionData.R

--- CancerStaging_LoadExpressionData.R
+++ CancerStaging_LoadExpressionData.R
@@ -134,6 +134,11 @@ merged_data_patient_info_count<-unique(merged_data_patient_info[which( merged_da
 
 # Cases per stage
 table_cases_per_stage<-table(merged_data_patient_info_count$project_id)
+
+# Write to to file the number of cases
+cat(paste("Number of cases: ", sep=""),file=results_files,sep="\n", append=TRUE)
+cat(paste(names(table_cases_per_stage), collapse=" "),file=results_files,sep="\n", append=TRUE)
+cat(paste(table_cases_per_stage, collapse=" "),file=results_files,sep="\n", append=TRUE)
 #####################################################################################################################
 # Organize how to send to Carles
 write_tsv(merged_data_patient_info, "/home/felipe/Documents/Cancer_staging/merged_data_patient_info.tsv")
