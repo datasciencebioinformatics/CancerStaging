6cc5d89ce8d1b48fc0ed1e11853ab6bcb8962da6
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-08 06:40:25 -0500

    Update CancerStaging_Filters.R

--- CancerStaging_Filters.R
+++ CancerStaging_Filters.R
@@ -3,13 +3,13 @@
 for (normalization_scheme in normalization_schemes)
 {
 	# First, I will load the statistic table   	
-	normalized_statistic_table<-read.table(file = paste("/home/felipe/Documents/Cancer_staging/df_statistics_all_projects_",normalization_scheme,".tsv",sep="") , sep = '\t', header = TRUE,fill=TRUE)
+	normalized_statistic_table<-list_logchange_tumor_control[[normalized_table_names]]
 	
 	# Select only tumor genes
 	tumor_genes<-normalized_statistic_table[normalized_statistic_table$tumor_genes == "yes","gene"]
 	
 	# First, I will load the expression table   	
-	normalized_expression_table<-df_reads_count_all_projects[[normalization_scheme]]
+	normalized_expression_table<-na.omit(df_reads_count_all_projects[[normalization_scheme]])
 	
 	# normalized_expression_table
 	normalized_expression_table<-normalized_expression_table[tumor_genes,]	
