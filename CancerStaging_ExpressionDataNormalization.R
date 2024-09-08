3bc480b5e8d70c3edb9f3767a966eed01c789e3a
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-08 06:17:58 -0500

    Update CancerStaging_ExpressionDataNormalization.R

--- CancerStaging_ExpressionDataNormalization.R
+++ CancerStaging_ExpressionDataNormalization.R
@@ -113,5 +113,4 @@ png(filename=paste(output_dir,"df_normalization.png",sep=""), width = 24, height
 dev.off()
 ####################################################################################
 df_reads_count_all_projects<-list(raw=df_reads_count_all_projects_raw,tmm=df_reads_count_all_projects_tmm,fpkm=df_reads_count_all_projects_fpkm,tpm=df_reads_count_all_projects_tpm,rpkm=df_reads_count_all_projects_rpkm)
-save(df_reads_count_all_projects, file = paste(output_dir,"/","ExpressionData.RData",sep=""))
 print("\nCancerStaging_ExpressionDataNormalization")
