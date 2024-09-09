3835166df3098b71cd2d08ed93ee2ce65de4e132
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-09 14:27:15 -0500

    Update CancerStaging_SetupAllParamters.R

--- CancerStaging_SetupAllParamters.R
+++ CancerStaging_SetupAllParamters.R
@@ -1,5 +1,5 @@
 # Set name of the TCGA project
-TCGA_project <- "TCGA-LUSC"
+TCGA_project <- "all"
 
 # Execute TCGA coomand
 t1 <- try(system(paste("mkdir /home/felipe/Documents/Cancer_staging/outputfolder/"), intern = TRUE))
