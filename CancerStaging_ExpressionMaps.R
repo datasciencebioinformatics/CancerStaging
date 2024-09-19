97e64d0648484350a0a4e4e9af36960a7471e013
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-19 09:27:33 -0500

    Update CancerStaging_ExpressionMaps.R

--- CancerStaging_ExpressionMaps.R
+++ CancerStaging_ExpressionMaps.R
@@ -1,4 +1,18 @@
-# Construnction of 3d coordinates
-# X=stages : merged_data_patient_info_merged
-# Y=connectivity : interactome_data
-# Z=expression : reads_count_all_projects
+# Consitency - check filters meticulously.
+# FPKM, TPM  - take these as robust.
+
+# Construnction of 3d coordinates.
+# X=stages       : merged_data_patient_info_merged  # Must be consistent.
+# Y=connectivity : interactome_data                 # Carels checked    .
+# Z=expression   : reads_count_all_projects         # Use the robust    .
+
+# Accross cancer types.
+# The hypohtoses is to put the clusters ordered by entropy in the 3d maps                   .
+# The hypotheseis is to order the clusters by entropy in the 3d map                         .
+# Amont the 2d coordinates this can have low signal, in a three map can have a stroger sinal.
+
+# October, paper article.
+# October.
+
+
+
