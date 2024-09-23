f62bd679b6fe06cf1227302d0e017dece04f5002
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-23 10:40:45 -0500

    Update CancerStaging_ExpressionMaps.R

--- CancerStaging_ExpressionMaps.R
+++ CancerStaging_ExpressionMaps.R
@@ -26,8 +26,30 @@ Interactomes_GC3_T2_merged<-merge(geneLength_ENTREZID_ENSEMBL,Interactomes_GC3_T
 # Amont the 2d coordinates this can have low signal, in a three map can have a stroger sinal.
 expression_table_normalized<-df_reads_count_all_projects[[normalization_scheme]]
 
-# October, paper article.
-# October.
+# I need take the intersection of ids from the interactome table and expression table.
+# The expression table has unique ids but it seems the Interactomes_GC3_T2_merged is duplicated (check).
+# Uniprot entries have multiple ENSEMBL. Criteria is to take UNIRPOTKB with highest conections.
+# To Do : take UNIRPOTKB with highest conections.
+
+# Set ensembl ids
+ENSEMBL_ids<-unique(intersect(rownames(expression_table_normalized),Interactomes_GC3_T2_merged$ENSEMBL))
+                                 
+# Take the used genes
+used_genes<-unique(rownames(expression_table_normalized[which(rownames(expression_table_normalized) %in% Interactomes_GC3_T2_merged$ENSEMBL),]))
+
+Interactomes_GC3_T2_merged[Interactomes_GC3_T2_merged$ENSEMBL %in% used_genes,]
+
+dim(expression_table_normalized[used_genes,])
+
+
+
+
+
+[Interactomes_GC3_T2_merged$ENSEMBL,]
+
+
+
+Interactomes_GC3_T2_merged<-merge(expression_table_normalized,Interactomes_GC3_T2_data,by="SYMBOL")
 
 
 
