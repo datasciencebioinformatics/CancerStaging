fec5972e57cebb49bcb90f01d08dd41bffb7c3cf
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-09-27 14:43:19 -0500

    Update CancerStaging_ExpressionDataNormalization_Carels.R

--- CancerStaging_ExpressionDataNormalization_Carels.R
+++ CancerStaging_ExpressionDataNormalization_Carels.R
@@ -43,7 +43,7 @@ geneLength_ENTREZID_ENSEMBL<-merge(store_gene_length,id_conversion_ENTREZID_ENSE
 cds_file <- "/home/felipe/Documents/github/CancerStaging/CDS_bp.txt"                                                  
 
 # Load data
-cds_data <-read.table(file = cds_file, sep = ' ', header = TRUE,fill=TRUE)    
+cds_data <-read.table(file = cds_file, sep = '\t', header = TRUE,fill=TRUE)    
 ####################################################################################################################
 # Then I will merge the dataset to obtain the ENSEMBL ids
 id_conversion_ENTREZID_ENSEMBL <-bitr(store_gene_length$ENTREZID, fromType = "ENTREZID", toType = c("SYMBOL","ENSEMBL","UNIPROT"), OrgDb="org.Hs.eg.db")
