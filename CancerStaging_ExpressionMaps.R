27aaec9aa30cab8095c86724da6181aea015f0c0
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-10-01 08:40:45 -0500

    Update CancerStaging_ExpressionMaps.R

--- CancerStaging_ExpressionMaps.R
+++ CancerStaging_ExpressionMaps.R
@@ -50,23 +50,39 @@ expression_table_normalized<-df_reads_count_all_projects[[normalization_scheme]]
 # Transform the Interactomes_GC3_T2_merged to as.data.table
 Interactomes_GC3_T2_merged<-as.data.table(Interactomes_GC3_T2_merged)
 
-# In the file "Interactomes_GC3_T2" each genes can have multiple entries with multiple values for the variable connections.
-# Only the occurance with greatest number of Conections will be used.
-# Interactomes_GC3_T2_merged
-Interactomes_GC3_T2_merged<-data.frame(Interactomes_GC3_T2_merged[Interactomes_GC3_T2_merged[, .I[which.max(Conections)], by=ENSEMBL]$V1])
-
 # Set ensembl ids
 ENSEMBL_ids<-unique(intersect(rownames(expression_table_normalized),Interactomes_GC3_T2_merged$ENSEMBL))
 
 # Selecte collumns to be extracted, Interactomes_GC3_T2_merged
 # "T2","GC3","Conections" and "ENSEMBL"
-Interactomes_GC3_T2_merged<-Interactomes_GC3_T2_merged[Interactomes_GC3_T2_merged$ENSEMBL %in% ENSEMBL_ids,c("T2","GC3","Conections","ENSEMBL")]
+Interactomes_GC3_T2_merged<-data.frame(Interactomes_GC3_T2_merged[which(Interactomes_GC3_T2_merged$ENSEMBL %in% ENSEMBL_ids),c("T2","GC3","Conections","ENSEMBL")])
+
+# In the file "Interactomes_GC3_T2" each genes can have multiple entries with multiple values for the variable connections.
+# Only the occurance with greatest number of Conections will be used.
+Interactomes_GC3_T2_df<-data.frame(T2=c(),GC3=c(),Conections=c(),ENSEMBL=c())
+
+# Interactomes_GC3_T2_merged
+for (gene_ENSEMBL in unique(Interactomes_GC3_T2_merged$ENSEMBL))
+{
+  # Take all entries of this 
+  Interactomes_GC3_T2_all<-Interactomes_GC3_T2_merged[which(Interactomes_GC3_T2_merged$ENSEMBL==gene_ENSEMBL),]
+
+  # Interactomes_GC3_T2_df
+  Interactomes_GC3_T2_df<-rbind(Interactomes_GC3_T2_df,Interactomes_GC3_T2_all[which.max(Interactomes_GC3_T2_all$Conections),])
+}
+# Replace the data.frames
+Interactomes_GC3_T2_merged<-Interactomes_GC3_T2_df
+
+# Set rownames
+rownames(Interactomes_GC3_T2_merged)<-Interactomes_GC3_T2_merged$ENSEMBL
 ####################################################################################################################################################
-# Set AveExp to zero 
+# Set AveExp to zero Interactomes_GC3_T2_merged
 Interactomes_GC3_T2_merged$AveExp<-0
 
 # Calculate the average expression for the epression of each g
 Interactomes_GC3_T2_merged[ENSEMBL_ids,"AveExp"]<-rowMeans(expression_table_normalized[ENSEMBL_ids,])
+
+Interactomes_GC3_T2_merged[ENSEMBL_ids,]
 
 # merged_expression_interactomes
 merged_expression_interactomes<-cbind(expression_table_normalized[ENSEMBL_ids,],Interactomes_GC3_T2_merged[Interactomes_GC3_T2_merged$ENSEMBL %in% ENSEMBL_ids,c("T2","GC3","Conections","ENSEMBL")])
@@ -95,16 +111,37 @@ png(filename=paste(output_dir,"geom_contour_melt.png",sep=""), width = 24, heigh
 dev.off()
 ####################################################################################################################################################
 Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_merged[,c("T2","GC3","AveExp","Conections")]
-Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_merged[!is.na(melt_expression_interactomes$Conections),]
-Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_merged[!is.na(melt_expression_interactomes$T2),]
-Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_merged[!is.na(melt_expression_interactomes$GC3),]
-Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_merged[Interactomes_GC3_T2_merged$AveExp>0,]
+Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_selected[!is.na(Interactomes_GC3_T2_selected$Conections),]
+Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_selected[!is.na(Interactomes_GC3_T2_selected$T2),]
+Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_selected[!is.na(Interactomes_GC3_T2_selected$GC3),]
+Interactomes_GC3_T2_selected                       <-Interactomes_GC3_T2_selected[Interactomes_GC3_T2_selected$AveExp>0,]
 
 # FindClusters_resolution
 png(filename=paste(output_dir,"geom_contour_merged.png",sep=""), width = 24, height = 24, res=600, units = "cm")  
-        scatterplot3d(Interactomes_GC3_T2_merged[,c("T2","GC3","AveExp")], pch = 16)
+        scatterplot3d(Interactomes_GC3_T2_selected[,c("T2","GC3","AveExp")], pch = 16) 
 dev.off()
 ####################################################################################################################################################
 # Plost histogram of T2, GC3 and AveExp 
 # Plost histogram of T2, GC3 and TPM
 ####################################################################################################################################################
+library(gridExtra)
+# Basic histogram
+hihstogram_T2       <-   ggplot(Interactomes_GC3_T2_merged, aes(x=T2))     + geom_histogram() #+  xlim(0, 250)
+hihstogram_GC3      <-   ggplot(Interactomes_GC3_T2_merged, aes(x=GC3))    + geom_histogram() #+  xlim(0, 250)
+hihstogram_AveExp   <-   ggplot(Interactomes_GC3_T2_merged, aes(x=AveExp)) + geom_histogram() #+  xlim(0, 250)
+# FindClusters_resolution
+png(filename=paste(output_dir,"Interactomes_GC3_T2_histogram.png",sep=""), width = 36, height = 12, res=600, units = "cm")  
+        grid.arrange(hihstogram_T2, hihstogram_GC3,hihstogram_AveExp, nrow = 1)
+dev.off()
+####################################################################################################################################################
+# Quick display of two cabapilities of GGally, to assess the distribution and correlation of variables 
+library(GGally)
+ 
+# Only Variable Labels on the outside (no axis labels)
+Interactomes_GC3_T2_plot <- ggpairs(Interactomes_GC3_T2_selected[,c("T2","GC3","AveExp")], axisLabels = "none")
+
+# FindClusters_resolution
+png(filename=paste(output_dir,"correaltion_matrix_GC3_T2_plot.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
+        Interactomes_GC3_T2_plot
+dev.off()
+
