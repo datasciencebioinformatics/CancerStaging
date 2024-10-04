0026075bfdad5cc33b71b49288a9b0be6861c760
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-10-04 06:38:27 -0500

    Update CancerStaging_ExpressionMaps.R

--- CancerStaging_ExpressionMaps.R
+++ CancerStaging_ExpressionMaps.R
@@ -76,8 +76,6 @@ rownames(Interactomes_GC3_T2_merged)<-Interactomes_GC3_T2_merged$ENSEMBL
 # FPKM, TPM  - take these as robust.
 # Paramter to set the normalization_scheme
 normalization_schemes<-c("tpm","fpkm","rpkm","tmm","tpm_calc")
-normalization_schemes<-c("tpm")
-
 
 # For each normlization normalization_scheme
 for (normalization_scheme in normalization_schemes)
