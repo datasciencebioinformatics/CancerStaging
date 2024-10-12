969794aa9c2f7956d1937d9486d6892a2bf3da14
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-10-12 08:40:13 -0500

    Update CancerStaging_SetupAllParamters.R

--- CancerStaging_SetupAllParamters.R
+++ CancerStaging_SetupAllParamters.R
@@ -9,7 +9,7 @@ tpm <- function(counts, lengths)
 }
 ####################################################################################################
 # Set name of the TCGA project
-TCGA_project <- "TCGA-PRAD"
+TCGA_project <- "ALL"
 
 # Execute TCGA coomand
 t1 <- try(system(paste("mkdir /home/felipe/Documents/Cancer_staging/outputfolder/"), intern = TRUE))
