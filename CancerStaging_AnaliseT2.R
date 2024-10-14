c1c0e9ab5694444c5fd2d35fab032c9723e8b0b1
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-10-14 14:48:47 -0500

    Create CancerStaging_AnaliseT2.R

---
+++ CancerStaging_AnaliseT2.R
@@ -0,0 +1,127 @@
+#######################################################################################################################
+#source("/home/felipe/Documents/github/CancerStaging/CancerStaging_SetupAllParamters.R")                               #
+#source("/home/felipe/Documents/github/CancerStaging/CancerStaging_LoadRPackages.R")                                   #
+#######################################################################################################################
+# saveRDS                                                                                                             #
+#saveRDS(object = df_reads_count_all_projects, file = paste(output_dir,"df_reads_count_all_projects.rds",sep=""))      #
+#saveRDS(object = geneLength_ENTREZID_ENSEMBL, file = paste(output_dir,"geneLength_ENTREZID_ENSEMBL.rds",sep=""))      #
+#saveRDS(object = Interactomes_GC3_T2_merged, file = paste(output_dir,"Interactomes_GC3_T2_merged.rds",sep=""))        #
+                                                                                                                      #
+# Restore the object                                                                                                  #
+#geneLength_ENTREZID_ENSEMBL<-readRDS(file = paste(output_dir,"geneLength_ENTREZID_ENSEMBL.rds",sep=""))               #
+#Interactomes_GC3_T2_merged <-readRDS(file = paste(output_dir,"Interactomes_GC3_T2_merged.rds",sep=""))                #
+#df_reads_count_all_projects<-readRDS(file = paste(output_dir,"df_reads_count_all_projects.rds",sep=""))               ###########################
+##merged_data_patient_info   <-read.table(file = "/home/felipe/Documents/Cancer_staging/merged_data_patient_info.tsv", sep = '\t', header = TRUE) #
+#################################################################################################################################################
+# Repeat for evey normalization scheme                                                                                                          #
+################################################################################################################################################# 
+# Construnction of 3d coordinates.
+# X=T2           : Thymine composition in second codon position (T2)
+# Y=connectivity : interactome_data                 # Carels checked    .
+# Z=expression   : reads_count_all_projects         # Use the fpkm
+# Reading the contents of TSV file using read_tsv() method
+Interactomes_GC3_T2_file <-"/home/felipe/Documents/github/CancerStaging/Interactomes_GC3_T2.csv"
+
+# Read the Interactomes_GC3_T2_data
+# First  collumn : UniprotKB
+# Second collumn : SYMBOL
+# Third  collumn : GC3
+# Fourth collumn : T2
+# Fifth  collumn : Conections
+Interactomes_GC3_T2_data <-read.table(file = Interactomes_GC3_T2_file, sep = '\t', header = TRUE,fill=TRUE) 
+
+# Set the second collum from GeneSymbol to SYMBOL
+colnames(Interactomes_GC3_T2_data)[2]<-"SYMBOL"
+
+# Merge Interactomes_GC3_T2 and geneLength_ENTREZID_ENSEMBL by gene SYMBOL.
+# geneLength_ENTREZID_ENSEMBL contains the following collumn:
+# ENTREZID   entrezid identifier
+# geneLength genelenght calculated by Carels
+# SYMBOL     gene symbol
+# ENSEMBL    ENSEMBL symbol
+Interactomes_GC3_T2_merged<-merge(geneLength_ENTREZID_ENSEMBL,Interactomes_GC3_T2_data,by="SYMBOL")
+
+# Transform the Interactomes_GC3_T2_merged to as.data.table
+Interactomes_GC3_T2_merged<-as.data.table(Interactomes_GC3_T2_merged)
+
+# Set ensembl ids
+ENSEMBL_ids<-unique(Interactomes_GC3_T2_merged$ENSEMBL)
+
+# Selecte collumns to be extracted, Interactomes_GC3_T2_merged
+# "T2","GC3","Conections" and "ENSEMBL"
+Interactomes_GC3_T2_merged<-data.frame(Interactomes_GC3_T2_merged[which(Interactomes_GC3_T2_merged$ENSEMBL %in% ENSEMBL_ids),c("T2","GC3","Conections","ENSEMBL")])
+
+# In the file "Interactomes_GC3_T2" each genes can have multiple entries with multiple values for the variable connections.
+# Only the occurance with greatest number of Conections will be used.
+Interactomes_GC3_T2_df<-data.frame(T2=c(),GC3=c(),Conections=c(),ENSEMBL=c())
+
+# Interactomes_GC3_T2_merged
+for (gene_ENSEMBL in unique(Interactomes_GC3_T2_merged$ENSEMBL))
+{
+  # Take all entries of this 
+  Interactomes_GC3_T2_all<-Interactomes_GC3_T2_merged[which(Interactomes_GC3_T2_merged$ENSEMBL==gene_ENSEMBL),]
+
+  # Interactomes_GC3_T2_df
+  Interactomes_GC3_T2_df<-rbind(Interactomes_GC3_T2_df,Interactomes_GC3_T2_all[which.max(Interactomes_GC3_T2_all$Conections),])
+}
+# Replace the data.frames
+Interactomes_GC3_T2_merged<-Interactomes_GC3_T2_df
+
+# Set rownames
+rownames(Interactomes_GC3_T2_merged)<-Interactomes_GC3_T2_merged$ENSEMBL
+
+####################################################################################################################################################
+# Interactomes_GC3_T2.csv file has 15650 entries. The number of annotated genes with gene length geneLength_ENTREZID_ENSEMBL is 14609. Among these, 14726 are common to Interactomes_GC3_T2 and geneLength_ENTREZID_ENSEMBL and will be used to create the maps. 
+# Consitency - check filters meticulously.
+# FPKM, TPM  - take these as robust.
+# Paramter to set the normalization_scheme
+normalization_schemes<-c("tpm","fpkm","tmm","rpkm","tpm_calc")
+normalization_schemes<-c("tpm","tmm")
+
+# For each normlization normalization_scheme
+for (normalization_scheme in normalization_schemes)
+{
+  #######################################################################################################################################
+  # Set the field stages
+  Interactomes_GC3_T2_merged$Stages<-"overllapping"
+  #######################################################################################################################################
+  # Path to files of selected_genes                                                                                                             # 
+  # genes_stages_I
+  selected_genes_Stage_I_data    <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_I",".tsv",sep=""), sep = '\t', header = TRUE) #
+  selected_genes_Stage_II_data   <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_II",".tsv",sep=""), sep = '\t', header = TRUE) #
+  selected_genes_Stage_III_data  <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_III",".tsv",sep=""), sep = '\t', header = TRUE) #
+
+  rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$gene
+  rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$gene
+  rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$gene
+
+  selected_genes_Stage_I_gene      <- selected_genes_Stage_I_data$gene
+  selected_genes_Stage_II_gene     <- selected_genes_Stage_II_data$gene
+  selected_genes_Stage_III_gene    <- selected_genes_Stage_III_data$gene
+  #######################################################################################################################################                                                                                                                                     #
+  unique_stage_I  =intersect(setdiff(selected_genes_Stage_I_gene, c(selected_genes_Stage_II_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_I_gene)
+  unique_stage_II =intersect(setdiff(selected_genes_Stage_II_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_II_gene)
+  unique_stage_III=intersect(setdiff(selected_genes_Stage_III_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_II_gene)),selected_genes_Stage_III_gene)
+  #######################################################################################################################################
+  Interactomes_GC3_T2_merged[unique_stage_I,"Stages"]<-"Stage I"
+  Interactomes_GC3_T2_merged[unique_stage_II,"Stages"]<-"Stage II"
+  Interactomes_GC3_T2_merged[unique_stage_III,"Stages"]<-"Stage III"
+
+  # Visualize: Specify the comparisons you want
+  my_comparisons <- list( c("Stage I", "Stage II"), c("Stage I", "Stage III"), c("Stage II", "Stage III"), c("Stage I", "overllapping"),c("Stage II", "overllapping"),c("Stage III", "overllapping"))
+  
+  # Add global p-value
+  # Add pairwise comparisons p-value
+  t2_with_outliers<-ggboxplot(Interactomes_GC3_T2_merged, x = "Stages", y = "T2",color = "Stages", palette = "jco")+  stat_compare_means(comparisons = my_comparisons, method = "t.test")+ stat_compare_means(label.y = 50)  + ggtitle(paste(TCGA_project,normalization_scheme,"T2",sep=" : "))
+  connections_with_outliers<-ggboxplot(Interactomes_GC3_T2_merged, x = "Stages", y = "Conections", color = "Stages", palette = "jco")+  stat_compare_means(comparisons = my_comparisons, method = "t.test")+ stat_compare_means(label.y = 50)  + ggtitle(paste(TCGA_project,normalization_scheme,"Conections",sep=" : "))
+  
+  # FindClusters_resolution
+  png(filename=paste(output_dir,"T2_Connection_Stat_",normalization_scheme,".png",sep=""), width = 28, height = 14, res=1200, units = "cm")
+    ggarrange(t2_with_outliers, connections_with_outliers, ncol=2, nrow=1, common.legend = TRUE, legend="bottom")
+  dev.off()  
+}
+
+
+
+
+  
