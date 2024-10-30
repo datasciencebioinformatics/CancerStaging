58cdfea49c0c5aabd8ec835f22454309ee9e0cee
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-10-30 13:11:48 -0500

    Update CancerStaging_ExpressionMapsStageSpecificRestore.R

--- CancerStaging_ExpressionMapsStageSpecificRestore.R
+++ CancerStaging_ExpressionMapsStageSpecificRestore.R
@@ -139,10 +139,8 @@ for (normalization_scheme in normalization_schemes)
 
 
     # FindClusters_resolution          
-    dev.off()
-    dev.off()    
     png(filename=paste(output_dir,"countour_T2_Coonections_melt_",normalization_scheme,"_",TCGA_project,"_Stage_all_boxplots.png",sep=""), width = 30, height = 15, res=600, units = "cm")      
-        m4
+        boxplots_plot
     dev.off()
     
     
@@ -289,10 +287,6 @@ for (normalization_scheme in normalization_schemes)
 
     # Arrange density plot
     boxplots_plot<-ggarrange(m4, m5, m6, m7, nrow = 1,ncol = 4, common.legend = TRUE, legend="bottom")    
-
-
-    dev.off()
-    dev.off()
 
     # FindClusters_resolution          
     png(filename=paste(output_dir,"countour_T2_Coonections_melt_",normalization_scheme,"_",TCGA_project,"_Stage_all_boxplots_perpatient.png",sep=""), width = 30, height = 15, res=600, units = "cm")  
