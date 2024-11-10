711ac5ab00cfceae51079164f9fc0636046f48bd
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-11-10 10:37:16 -0500

    Update CancerStaging_Hub.R

--- CancerStaging_Hub.R
+++ CancerStaging_Hub.R
@@ -12,10 +12,11 @@ rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$gene
 
 selected_genes_Stage_I_gene      <- selected_genes_Stage_I_data$gene
 selected_genes_Stage_II_gene     <- selected_genes_Stage_II_data$gene
-selected_genes_Stage_III_gene    <- selected_genes_Stage_III_data$gene  #######################################################################################################################################                                                                                                                                     #
-  unique_stage_I  =intersect(setdiff(selected_genes_Stage_I_gene, c(selected_genes_Stage_II_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_I_gene)
-  unique_stage_II =intersect(setdiff(selected_genes_Stage_II_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_II_gene)
-  unique_stage_III=intersect(setdiff(selected_genes_Stage_III_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_II_gene)),selected_genes_Stage_III_gene)
+selected_genes_Stage_III_gene    <- selected_genes_Stage_III_data$gene  
+#######################################################################################################################################                                                                                                                                     #
+unique_stage_I  =intersect(setdiff(selected_genes_Stage_I_gene, c(selected_genes_Stage_II_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_I_gene)
+unique_stage_II =intersect(setdiff(selected_genes_Stage_II_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_III_gene)),selected_genes_Stage_II_gene)
+unique_stage_III=intersect(setdiff(selected_genes_Stage_III_gene, c(selected_genes_Stage_I_gene,selected_genes_Stage_II_gene)),selected_genes_Stage_III_gene)
 
 # Considering that the average connection score of up-regulated genes across the 1722 of the three stages combined was 50.06 by reference to the IntAct reactome,
 intersect_conectivity<-connectivity[names(connectivity) %in% unique(c(selected_genes_Stage_I_gene,selected_genes_Stage_II_gene,selected_genes_Stage_III_gene))]
@@ -27,7 +28,6 @@ intersect_conectivity<-intersect_conectivity[which(intersect_conectivity>75)]
 
 # Because therapy is intended to maximize the patient comfort we also filtered out genes whose expression was larger than ~10 in the control since targeting drugs could affect the healthy tissue in case of basal expression.
 selected_genes<-df_FC[df_FC$Mean_normal<=10,]
-
 
 # Store conectivity
 df_selected_conectivity<-data.frame(intersect_conectivity[names(intersect_conectivity) %in% selected_genes$Gene])
@@ -63,3 +63,46 @@ df_selected_conectivity<-df_selected_conectivity[,c("Gene","Avg.normal", "sd.nor
 
 # Save TSV file with genes from Stage3
 write_tsv(df_selected_conectivity, paste(output_dir,"/selected_conectivity.tsv",sep=""))
+
+#######################################################################################################################################
+# Path to files of selected_genes                                                                                                             # 
+# genes_stages_I
+selected_genes_Stage_I_data    <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_I",".tsv",sep=""), sep = '\t', header = TRUE) #
+selected_genes_Stage_II_data   <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_II",".tsv",sep=""), sep = '\t', header = TRUE) #
+selected_genes_Stage_III_data  <-read.table(file = paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_","sample_stage_III",".tsv",sep=""), sep = '\t', header = TRUE) #
+
+rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$gene
+rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$gene
+rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$gene
+
+selected_genes_Stage_I_gene      <- selected_genes_Stage_I_data$gene
+selected_genes_Stage_II_gene     <- selected_genes_Stage_II_data$gene
+selected_genes_Stage_III_gene    <- selected_genes_Stage_III_data$gene  
+
+stage_I_II    =unique(intersect(selected_genes_Stage_I_gene,selected_genes_Stage_II_gene))
+stage_I_III   =unique(intersect(selected_genes_Stage_I_gene,selected_genes_Stage_III_gene))
+stage_II_III  =unique(intersect(selected_genes_Stage_II_gene,selected_genes_Stage_III_gene))
+
+
+
+# Considering that the average connection score of up-regulated genes across the 1722 of the three stages combined was 50.06 by reference to the IntAct reactome,
+intersect_conectivity_stage_I_II   <-connectivity[names(connectivity) %in% stage_I_II]
+intersect_conectivity_stage_I_III  <-connectivity[names(connectivity) %in% stage_I_III]
+intersect_conectivity_stage_II_III  <-connectivity[names(connectivity) %in% stage_II_III]
+
+# we chose 50 as a threshold above which to consider a protein with a higher connection score as a hub. 
+intersect_conectivity_stage_I_II <-intersect_conectivity_stage_I_II[which(intersect_conectivity_stage_I_II>75)]
+intersect_conectivity_stage_I_III<-intersect_conectivity_stage_I_III[which(intersect_conectivity_stage_I_III>75)]
+intersect_conectivity_stage_II_III<-intersect_conectivity_stage_II_III[which(intersect_conectivity_stage_II_III>75)]
+
+
+
+# Store conectivity
+df_selected_conectivity_stage_I_II<-data.frame(intersect_conectivity[names(intersect_conectivity_stage_I_II) %in% selected_genes$Gene])
+df_selected_conectivity_stage_I_III<-data.frame(intersect_conectivity[names(intersect_conectivity_stage_I_III) %in% selected_genes$Gene])
+df_selected_conectivity_stage_I_III<-data.frame(intersect_conectivity[names(intersect_conectivity_stage_OI_III) %in% selected_genes$Gene])
+
+
+# Store conectivity
+df_selected_conectivity<-data.frame(intersect_conectivity[names(intersect_conectivity) %in% selected_genes$Gene])
+
