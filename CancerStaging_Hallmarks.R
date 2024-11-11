68dcf9939970c9fb2d679debd5b94d37f7d4d037
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-11-11 12:05:25 -0500

    Update CancerStaging_Hallmarks.R

--- CancerStaging_Hallmarks.R
+++ CancerStaging_Hallmarks.R
@@ -1,1 +1,14 @@
+# Install msigdb packages.
+# retrieeve the hallmarks gene sets
+# subsetCollection(msigdb.hs, 'h')
+# check the hallmarks against the paper
 
+
+# Perform gene set enrichment analysis
+# Genes from stage I     (numGene, n)    : number of genes of this stage associated to Hallmark X.
+# Genes from stage I     (%)             : percentage of genes of this stage associated to Hallmark X.
+# Genes from stage II    (numGene, n)    : number of genes of this stage associated to Hallmark X.
+# Genes from stage II    (%)             : percentage of genes of this stage associated to Hallmark X.
+# Genes from stage III   (numGene, n)    : number of genes of this stage associated to Hallmark X.
+# Genes from stage III   (%)             : percentage of genes of this stage associated to Hallmark X.
+# Stage III minus I      (Δ)             : number of genes enriched in stage III minus number of genes enriched in stage I
