1593bfe57aa2815a217b05022fb77953dbbac909
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-11-17 14:19:41 -0500

    Update CancerStaging_FindStageSpecificGenes.R

--- CancerStaging_FindStageSpecificGenes.R
+++ CancerStaging_FindStageSpecificGenes.R
@@ -19,6 +19,7 @@ data.frame(Stage_i="sample_stage_III",Stage_ii="sample_normal"))                
 list_of_comparisson=list(sample_stage_I=sample_stage_I,sample_stage_II=sample_stage_II,sample_stage_III=sample_stage_III, sample_normal=sample_normal)
 #######################################################################################################################################
 list_stage_specific_genes<-c()
+list_stage_specific_genes_all<-c()
 ####################################################################################################################
 # for each  normalization scheme
 # Paramter to set the normalization_scheme
@@ -100,8 +101,7 @@ for (normalization_scheme in normalization_schemes)
 		#selected_genes<-log2change_Stage_i[ which(log2change_Stage_i$log2change>=threshold_stage),"gene"]	
 		####################################################################################################################	
 		# Save TSV file with genes from Stage3
-		write_tsv(na.omit(log2change_Stage_i[selected_genes,]), paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_",Stage_i,".tsv",sep=""))			
-		write_tsv(na.omit(log2change_Stage_i), paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_",Stage_i,"_all_genes.tsv",sep=""))			
+		write_tsv(na.omit(log2change_Stage_i[selected_genes,]), paste(output_dir,"/FindStageSpecificGenes_",normalization_scheme,"_",Stage_i,".tsv",sep=""))					
 		####################################################################################################################		
 		list_stage_specific_genes[[paste(normalization_scheme,"_",substring(Stage_i,8,20),sep="")]]<-log2change_Stage_i
 		####################################################################################################################	
