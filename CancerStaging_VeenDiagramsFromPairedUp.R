c9c45dcee5bf844b14258483efe2c2cc99cc1eff
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-11-19 13:09:08 -0500

    Update CancerStaging_VeenDiagramsFromPairedUp.R

--- CancerStaging_VeenDiagramsFromPairedUp.R
+++ CancerStaging_VeenDiagramsFromPairedUp.R
@@ -30,7 +30,31 @@ for (normalization_scheme in normalization_schemes)
   #######################################################################################################################################
   # Select stages 
   stages_I_II_III_unique<-ggVennDiagram(list(Stage_I=unique_stage_I,Stage_II=unique_stage_II,Stage_III=unique_stage_III), label_alpha = 0.9,set_color = c("grey50","grey50","grey50")) +  scale_fill_gradient(low = "white", high = "white") + theme_bw() + ggtitle("Stages I, II and III")+ guides(fill="none")
-  stages_I_II_III<-ggVennDiagram(list(Stage_I=selected_genes_Stage_I_data,Stage_II=selected_genes_Stage_II_data,Stage_III=selected_genes_Stage_III_data), label_alpha = 0.9,set_color = c("grey50","grey50","grey50")) + scale_fill_gradient(low = "white", high = "white") + theme_bw() + ggtitle("Stages I, II and III")+ guides(fill="none")
+  stages_I_II_III_unique<-ggVennDiagram(list(Stage_I=selected_genes_Stage_I_gene,Stage_II=selected_genes_Stage_II_gene,Stage_III=selected_genes_Stage_III_gene), label_alpha = 0.9,set_color = c("grey50","grey50","grey50")) +  scale_fill_gradient(low = "white", high = "white") + theme_bw() + ggtitle("Stages I, II and III")+ guides(fill="none")
+
+  # Subset taqble
+  merged_data_patient_analysis<-merged_data_patient_info[merged_data_patient_info$stages %in% c("Stage I","Stage II","Stage III"),]
+
+  # Split normal and tumor samples
+  tumor_samnples<-unique(data.frame(sample_id=merged_data_patient_analysis[merged_data_patient_analysis$tissue_type=="Tumor","sample_id",""],tissue_type="Tumor"))
+  normal_samnples<-unique(data.frame(sample_id=merged_data_patient_analysis[merged_data_patient_analysis$tissue_type=="Normal","sample_id"],tissue_type="Normal"))
+
+  # Merge tumor and normal and samples
+  tissue_type<-rbind(tumor_samnples,normal_samnples)
+
+  # Melt data.frame
+  melt_normalized_table_pca<-melt(normalized_table_pca)
+
+  # Rename colllumns
+  colnames(melt_normalized_table_pca)<-c("ENSEMBL","sample_id","TPM")
+
+  # Merge information
+  melt_normalized_table_pca<-merge(melt_normalized_table_pca,tissue_type,by="sample_id")
+
+
+  pca_res_df_genes_tumor <- prcomp(melt_normalized_table_pca[,c("TPM")], scale. = TRUE) 
+  autoplot(pca_res_df_genes_tumor, data = melt_normalized_table_pca, colour = 'tissue_type')
+
   
   # FindClusters_resolution
   png(filename=paste(output_dir,"Ven_Diagrams.png",sep=""), width = 28, height = 14, res=600, units = "cm")
