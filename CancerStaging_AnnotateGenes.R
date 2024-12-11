37a204ed7402e8d5957915e93f816ca7e1cbb00c
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-12-11 08:44:30 -0500

    Update CancerStaging_AnnotateGenes.R

--- CancerStaging_AnnotateGenes.R
+++ CancerStaging_AnnotateGenes.R
@@ -1,4 +1,10 @@
-########################################################################################################################
+###########################################################################################################
+# Restore the object                                                                                      #
+Interactomes_GC3_T2_merged <-readRDS(file = paste(output_dir,"Interactomes_GC3_T2_merged.rds",sep=""))    #
+normalization_schemes      <-readRDS(file = paste(output_dir,"normalization_schemes.rds",sep=""))         #
+df_reads_count_all_projects<-readRDS(file = paste(output_dir,"df_reads_count_all_projects.rds",sep=""))   #
+list_of_comparisson        <-readRDS(file = paste(output_dir,"list_of_comparisson.rds",sep=""))           #
+###########################################################################################################
 # Set rowanames
 rownames(df_FC)<-rownames(df_FC)
 
@@ -23,11 +29,28 @@ colnames(Interactomes_GC3_T2_merged)[4]<-"ensembl_gene_id"
 # Set genes_rankData_stage_all_genes
 genes_rankData_stage_all_genes_merged<-merge(Interactomes_GC3_T2_merged, genes_rankData_stage_all_genes,by="ensembl_gene_id")
 
-# genes_rankData_stage_all_genes_merged
-write_tsv(genes_rankData_stage_all_genes_merged,   paste(output_dir,"/Table_S3.tsv",sep=""))
-
 # Format table 8
-Table3<-genes_rankData_stage_all_genes[genes_rankData_stage_all_genes$FC>=50 & genes_rankData_stage_all_genes$Mean_normal<=10,]
+Table8<-genes_rankData_stage_all_genes_merged[genes_rankData_stage_all_genes_merged$FC>=50 & genes_rankData_stage_all_genes_merged$Mean_normal<=10,c("hgnc_symbol","Mean_normal","sd_normal","Mean_tumor","sd_tumor","FC","Conections")]
 
 # Save TSV file with genes from Stage3
-write_tsv(Table3, paste(output_dir,"/Table8.tsv",sep=""))
+write_tsv(Table8, paste(output_dir,"/Table8.tsv",sep=""))
+#########################################################################################################
+# Format table S3
+genes_rankData_stage_all_genes_merged$FC5 <-"NO"
+genes_rankData_stage_all_genes_merged$FC10<-"NO"
+genes_rankData_stage_all_genes_merged$FC20<-"NO"
+genes_rankData_stage_all_genes_merged$FC30<-"NO"
+genes_rankData_stage_all_genes_merged$FC40<-"NO"
+genes_rankData_stage_all_genes_merged$FC50<-"NO"
+
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=5),"FC5"]   <-"YES"
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=10),"FC10"] <-"YES"
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=20),"FC20"] <-"YES"
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=30),"FC30"] <-"YES"
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=40),"FC40"] <-"YES"
+genes_rankData_stage_all_genes_merged[which(genes_rankData_stage_all_genes_merged$FC>=50),"FC50"] <-"YES"
+#########################################################################################################
+# genes_rankData_stage_all_genes_merged
+write_tsv(genes_rankData_stage_all_genes_merged,   paste(output_dir,"/Table_S3.tsv",sep=""))
+#########################################################################################################
+
