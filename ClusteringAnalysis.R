29e76a9c91937e5d03e888905cfe24fb12b925c9
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-14 13:25:29 -0500

    Update ClusteringAnalysis.R

--- ClusteringAnalysis.R
+++ ClusteringAnalysis.R
@@ -1,16 +1,31 @@
 # Add a collumns with the sample id
 transporse_normalized_table$sample_id<-rownames(transporse_normalized_table)
 
-# Merge transporse_normalized_table with metadata
-transporse_normalized_table<-merge(transporse_normalized_table,merged_data_patient_info,by="sample_id")
+# Merge transporse_normalized_table with metadatamerged_data_patient_info
+transporse_normalized_table<-unique(merge(transporse_normalized_table,merged_data_patient_info,by="sample_id")[,c(genes,"tissue_type","sample_id")])
 
-transporse_normalized_table<-transporse_normalized_table[patient,c(genes,"tissue_type")]
+# set rownames
+rownames(transporse_normalized_table)<-transporse_normalized_table$sample_id
+
+# Split into trainning and testing
+trainning <- sample_n(transporse_normalized_table,dim(transporse_normalized_table)[1]/2)
+testing   <- transporse_normalized_table[which(!rownames(transporse_normalized_table) %in% rownames(trainning)),]
+
+# Basic Parameter Tuning
+fitControl <- trainControl(method = "repeatedcv",
+                           number = 10,
+                           repeats = 10,
+                           ## Estimate class probabilities
+                           classProbs = TRUE,
+                           ## Evaluate performance using 
+                           ## the following function
+                           summaryFunction = twoClassSummary)
 
 # model 3 logistic regression
-mod3 <- train(tissue_type ~ ., method = 'glm', data = train, trControl = tCtrl, preProcess = c('center', 'scale'))
+mod3 <- train(tissue_type ~ ., method = 'glm', data = trainning, trControl = fitControl, preProcess = c('center', 'scale'))
 
 # predict with mod 3
-pred3 <- predict(mod3, newdata = test)
+pred3 <- predict(mod3, newdata = testing)
 
 # confusionMatrix with mod 3
 cfm3 <- confusionMatrix(pred3, test$booking_status)
